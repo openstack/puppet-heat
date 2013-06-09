adf5af8b9fb2675237b89d551360571230d6a460
Émilien Macchi <emilien.macchi@enovance.com>
2013-06-09 23:56:26 +0200

    Fix api.pp with RabbitMQ

--- manifests/api.pp
+++ manifests/api.pp
@@ -14,7 +14,7 @@ class heat::api (
   $bind_port         = '8004',
   $verbose           = 'False',
   $debug             = 'False',
-  $rabbit_hosts      = '127.0.0.1',
+  $rabbit_host       = '127.0.0.1',
 ) {
 
   include heat::params
@@ -76,7 +76,7 @@ class heat::api (
     'DEFAULT/log_dir'                : value => $::heat::params::log_dir;
     'DEFAULT/bind_host'              : value => $bind_host;
     'DEFAULT/bind_port'              : value => $bind_port;
-    'DEFAULT/rabbit_hosts'           : value => $rabbit_hosts;
+    'DEFAULT/rabbit_host '           : value => $rabbit_host;
     'ec2authtoken/keystone_ec2_uri'  : value => $keystone_ec2_uri;
     'ec2authtoken/auth_uri'          : value => $auth_uri;
     'keystone_authtoken/auth_host'         : value => $keystone_host;

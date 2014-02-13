498ce5093c272f20f305420389ca419934ecf98e
Emilien Macchi <emilien.macchi@enovance.com>
2014-02-13 13:36:31 +0100

    api: bring workers support
    Allow to configure the number of workers for Heat API services.
    
    Change-Id: I94f938ede1f7f5e97adb406d592206e667d30f8d
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/classes/heat_api_cfn_spec.rb
+++ spec/classes/heat_api_cfn_spec.rb
@@ -5,7 +5,8 @@ describe 'heat::api_cfn' do
   let :params do
     {
       :bind_host => '127.0.0.1',
-      :bind_port => '1234'
+      :bind_port => '1234',
+      :workers   => '0'
     }
   end
 
@@ -20,6 +21,7 @@ describe 'heat::api_cfn' do
 
     it { should contain_heat_config('heat_api_cfn/bind_host').with_value( params[:bind_host] ) }
     it { should contain_heat_config('heat_api_cfn/bind_port').with_value( params[:bind_port] ) }
+    it { should contain_heat_config('heat_api_cfn/workers').with_value( params[:workers] ) }
 
   end
 

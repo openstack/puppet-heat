61c332248717585425127b21cf7485e7dff64a1c
Daneyon Hansen <danehans@cisco.com>
2014-08-14 19:33:24 +0000

    Updates Example Manifest
    The existing example manifest does not work. This patch updates
    the example manifest so users can easily deploy Heat using Puppet.
    
    Change-Id: I5b6f19760a7b629e0151f895ab4532af804a8af0

--- examples/site.pp
+++ examples/site.pp
@@ -1,29 +1,58 @@
+### Begin top scope parameters ###
+$configure_database     = true
+$configure_keystone     = true
+$configure_heat         = true
+$region                 = 'RegionOne'
+$keystone_host          = '127.0.0.1'
+$keystone_password      = 'changeme'
+$db_host                = '127.0.0.1'
+$db_password            = 'changeme'
+$db_package_name        = undef
+$auth_encryption_key    = 'whatever-key-you-like'
+$heat_keystone_password = 'changeme'
+$heat_host              = '127.0.0.1'
+### End top scope parameters ###
+
 node default {
   Exec {
     path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
   }
 
-  # First, install a mysql server
-  class { 'mysql::server': }
-
-  # And create the database
-  class { 'heat::db::mysql':
-    password => 'heat',
+  if ($configure_database) {
+    # Install MySQL or MariaDB
+    class { 'mysql::server':
+      package_name => $db_package_name,
+    }
+    class { 'heat::db::mysql':
+      mysql_module => '2.2',
+      host         => $db_host,
+      password     => $db_password,
+    }
   }
 
-  # Common class
-  class { 'heat':
-    # The keystone_password parameter is mandatory
-    keystone_password => 'password',
-    sql_connection    => 'mysql://heat:heat@localhost/heat'
+  if ($configure_keystone) {
+    # Configure Keystone for Heat support
+    class { 'heat::keystone::auth':
+      password         => $heat_keystone_password,
+      public_address   => $heat_host,
+      admin_address    => $heat_host,
+      internal_address => $heat_host,
+    }
   }
 
-  # Install heat-engine
-  class { 'heat::engine':
-    auth_encryption_key => 'whatever-key-you-like',
+  if ($configure_heat) {
+    # Common class
+    class { 'heat':
+      mysql_module      => '2.2',
+      keystone_host     => $keystone_host,
+      keystone_password => $keystone_password,
+      sql_connection    => "mysql://heat:${db_password}@${db_host}/heat",
+    }
+    # Install heat-engine
+    class { 'heat::engine':
+      auth_encryption_key => $auth_encryption_key,
+    }
+    # Install the heat-api service
+    class { 'heat::api': }
   }
-
-  # Install the heat-api service
-  class { 'heat::api': }
-
 }

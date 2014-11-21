077ef2d42c150c6d4094e096eb1d0f82a3f3c420
Emilien Macchi <emilien.macchi@enovance.com>
2014-11-21 10:21:19 -0500

    Refactorise Keystone resources management
    Refactorise the code of Keystone resources management with backward
    compatibility since we don't modify the unit tests.
    
    Change-Id: I397768ba0c9d8020c6e722aa34315dd32b1d967a
    Implements: blueprint common-openstack-identity-resource

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -107,40 +107,29 @@ class heat::keystone::auth (
     $real_service_name = $service_name
   }
 
-  if $configure_user {
-    keystone_user { $auth_name:
-      ensure   => present,
-      password => $password,
-      email    => $email,
-      tenant   => $tenant,
-    }
+  keystone::resource::service_identity { $auth_name:
+    configure_user      => $configure_user,
+    configure_user_role => $configure_user_role,
+    configure_endpoint  => $configure_endpoint,
+    service_type        => $service_type,
+    service_description => 'Openstack Orchestration Service',
+    service_name        => $real_service_name,
+    region              => $region,
+    password            => $password,
+    email               => $email,
+    tenant              => $tenant,
+    public_url          => "${public_protocol}://${public_address}:${port}/${version}/%(tenant_id)s",
+    admin_url           => "${admin_protocol}://${admin_address}:${port}/${version}/%(tenant_id)s",
+    internal_url        => "${internal_protocol}://${internal_address}:${port}/${version}/%(tenant_id)s",
   }
 
   if $configure_user_role {
     Keystone_user_role["${auth_name}@${tenant}"] ~>
       Service <| name == 'heat-api' |>
-
-    keystone_user_role { "${auth_name}@${tenant}":
-      ensure => present,
-      roles  => ['admin'],
-    }
   }
 
   keystone_role { 'heat_stack_user':
         ensure => present,
   }
 
-  keystone_service { $real_service_name:
-    ensure      => present,
-    type        => $service_type,
-    description => 'Openstack Orchestration Service',
-  }
-  if $configure_endpoint {
-    keystone_endpoint { "${region}/${real_service_name}":
-      ensure       => present,
-      public_url   => "${public_protocol}://${public_address}:${port}/${version}/%(tenant_id)s",
-      admin_url    => "${admin_protocol}://${admin_address}:${port}/${version}/%(tenant_id)s",
-      internal_url => "${internal_protocol}://${internal_address}:${port}/${version}/%(tenant_id)s",
-    }
-  }
 }

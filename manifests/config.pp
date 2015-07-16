1b1f7ca0ab9d25f7e487bfbc1b1628887a0d4a7c
Yanis Guenane <yguenane@redhat.com>
2015-07-16 15:26:14 +0200

    Add heat::config class
    Other modules provide an X::config class to be able to specify
    parameters that are not yet part of the module. Heat was missing this
    feature.
    
    This commit aims to add it for puppet-heat.
    
    Change-Id: Ie481598ee4a1b2c451a1e8bddd5faa526d15ef30

---
+++ manifests/config.pp
@@ -0,0 +1,30 @@
+# == Class: heat::config
+#
+# This class is used to manage arbitrary Heat configurations.
+#
+# === Parameters
+#
+# [*heat_config*]
+#   (optional) Allow configuration of arbitrary Heat configurations.
+#   The value is an hash of heat_config resources. Example:
+#   { 'DEFAULT/foo' => { value => 'fooValue'},
+#     'DEFAULT/bar' => { value => 'barValue'}
+#   }
+#   In yaml format, Example:
+#   heat_config:
+#     DEFAULT/foo:
+#       value: fooValue
+#     DEFAULT/bar:
+#       value: barValue
+#
+#   NOTE: The configuration MUST NOT be already handled by this module
+#   or Puppet catalog compilation will fail with duplicate resources.
+#
+class heat::config (
+  $heat_config = {},
+) {
+
+  validate_hash($heat_config)
+
+  create_resources('heat_config', $heat_config)
+}

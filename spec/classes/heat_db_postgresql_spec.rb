1e4d19aeae7ee4952900901175000a1eedc9fa33
Maksym Yatsenko <maxy.1982@gmail.com>
2016-10-10 18:38:13 +0300

    Remove '::processorcount' fact from tests.
    As '::processorcount' fact is not used anymore,
    it was removed from tests.
    
    Change-Id: I76f451d17ae997f6ca7c670a300eb319e6964f5a

--- spec/classes/heat_db_postgresql_spec.rb
+++ spec/classes/heat_db_postgresql_spec.rb
@@ -33,7 +33,6 @@ describe 'heat::db::postgresql' do
     context "on #{os}" do
       let (:facts) do
         facts.merge(OSDefaults.get_facts({
-          :processorcount => 8,
           :concat_basedir => '/var/lib/puppet/concat'
         }))
       end

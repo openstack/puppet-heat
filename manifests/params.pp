a6a860106bb22bb17c581e0908d10be612d7d619
Xingchao Yu <yuxcer@gmail.com>
2016-11-04 12:28:55 +0800

    Align equals in heat::params
    Change-Id: Ia94cd822715bf75b9eebb876e17d0b05fdf0da50

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,31 +8,31 @@ class heat::params {
   case $::osfamily {
     'RedHat': {
       # package names
-      $api_package_name = 'openstack-heat-api'
+      $api_package_name            = 'openstack-heat-api'
       $api_cloudwatch_package_name = 'openstack-heat-api-cloudwatch'
-      $api_cfn_package_name = 'openstack-heat-api-cfn'
-      $engine_package_name = 'openstack-heat-engine'
-      $client_package_name = 'python-heatclient'
-      $common_package_name = 'openstack-heat-common'
+      $api_cfn_package_name        = 'openstack-heat-api-cfn'
+      $engine_package_name         = 'openstack-heat-engine'
+      $client_package_name         = 'python-heatclient'
+      $common_package_name         = 'openstack-heat-common'
       # service names
-      $api_service_name = 'openstack-heat-api'
+      $api_service_name            = 'openstack-heat-api'
       $api_cloudwatch_service_name = 'openstack-heat-api-cloudwatch'
-      $api_cfn_service_name = 'openstack-heat-api-cfn'
-      $engine_service_name = 'openstack-heat-engine'
+      $api_cfn_service_name        = 'openstack-heat-api-cfn'
+      $engine_service_name         = 'openstack-heat-engine'
     }
     'Debian': {
       # package names
-      $api_package_name = 'heat-api'
+      $api_package_name            = 'heat-api'
       $api_cloudwatch_package_name = 'heat-api-cloudwatch'
-      $api_cfn_package_name = 'heat-api-cfn'
-      $engine_package_name = 'heat-engine'
-      $client_package_name = 'python-heatclient'
-      $common_package_name = 'heat-common'
+      $api_cfn_package_name        = 'heat-api-cfn'
+      $engine_package_name         = 'heat-engine'
+      $client_package_name         = 'python-heatclient'
+      $common_package_name         = 'heat-common'
       # service names
-      $api_service_name = 'heat-api'
+      $api_service_name            = 'heat-api'
       $api_cloudwatch_service_name = 'heat-api-cloudwatch'
-      $api_cfn_service_name = 'heat-api-cfn'
-      $engine_service_name = 'heat-engine'
+      $api_cfn_service_name        = 'heat-api-cfn'
+      $engine_service_name         = 'heat-engine'
       # Operating system specific
       case $::operatingsystem {
         'Ubuntu': {

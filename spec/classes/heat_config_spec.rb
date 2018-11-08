ffc4e32f400693ff32350c14949a9b0d2e1373a0
Tobias Urdin <tobias.urdin@binero.se>
2018-11-08 11:15:49 +0100

    Convert spec testing to rspec-puppet-facts
    Change-Id: I57fe44d8d6bc03d7560f68024c6af85a09936c92

--- spec/classes/heat_config_spec.rb
+++ spec/classes/heat_config_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'heat::config' do
 
   let :params do
-    { :heat_config => {
+    {
+      :heat_config        => {
         'DEFAULT/foo' => { 'value'  => 'fooValue' },
         'DEFAULT/bar' => { 'value'  => 'barValue' },
         'DEFAULT/baz' => { 'ensure' => 'absent' }
@@ -16,18 +17,31 @@ describe 'heat::config' do
     }
   end
 
-  it { is_expected.to contain_class('heat::deps') }
+  shared_examples 'heat::config' do
+    it { should contain_class('heat::deps') }
 
-  it 'configures arbitrary heat configurations' do
-    is_expected.to contain_heat_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_heat_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_heat_config('DEFAULT/baz').with_ensure('absent')
+    it {
+      should contain_heat_config('DEFAULT/foo').with_value('fooValue')
+      should contain_heat_config('DEFAULT/bar').with_value('barValue')
+      should contain_heat_config('DEFAULT/baz').with_ensure('absent')
+    }
+
+    it {
+      should contain_heat_api_paste_ini('DEFAULT/foo2').with_value('fooValue')
+      should contain_heat_api_paste_ini('DEFAULT/bar2').with_value('barValue')
+      should contain_heat_api_paste_ini('DEFAULT/baz2').with_ensure('absent')
+    }
   end
 
-  it 'configures arbitrary heat-api-paste configurations' do
-    is_expected.to contain_heat_api_paste_ini('DEFAULT/foo2').with_value('fooValue')
-    is_expected.to contain_heat_api_paste_ini('DEFAULT/bar2').with_value('barValue')
-    is_expected.to contain_heat_api_paste_ini('DEFAULT/baz2').with_ensure('absent')
-  end
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
 
+      it_behaves_like 'heat::config'
+    end
+  end
 end

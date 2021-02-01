72d1b4d137b0b610fdf879bcd13efd188cfb25fb
Takashi Kajinami <tkajinam@redhat.com>
2021-02-01 17:29:53 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Change-Id: I3a09b3dcdbece397edb19051683657bec2d411c4

---
+++ spec/classes/heat_healthcheck_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe 'heat::healthcheck' do
+
+  shared_examples_for 'heat::healthcheck' do
+
+    context 'with default parameters' do
+      let :params do
+        {}
+      end
+
+      it 'configures default values' do
+        is_expected.to contain_oslo__healthcheck('heat_config').with(
+          :detailed              => '<SERVICE DEFAULT>',
+          :backends              => '<SERVICE DEFAULT>',
+          :disable_by_file_path  => '<SERVICE DEFAULT>',
+          :disable_by_file_paths => '<SERVICE DEFAULT>',
+        )
+      end
+    end
+
+    context 'with specific parameters' do
+      let :params do
+        {
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/heat/healthcheck/disabled',
+          :disable_by_file_paths => ['8004:/etc/heat/healthcheck/disabled'],
+        }
+      end
+
+      it 'configures specified values' do
+        is_expected.to contain_oslo__healthcheck('heat_config').with(
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/heat/healthcheck/disabled',
+          :disable_by_file_paths => ['8004:/etc/heat/healthcheck/disabled'],
+        )
+      end
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'heat::healthcheck'
+    end
+  end
+
+end

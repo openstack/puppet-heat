2d1311f1c60ebc627fb2d117ceaa9b59dc149724
Takashi Kajinami <tkajinam@redhat.com>
2021-11-25 16:16:32 +0900

    cfn: Do not test detail of keystone::resource::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: I68c8a1870c2940e2934ca3b11a71a75f5db179c2

--- spec/classes/heat_keystone_auth_cfn_spec.rb
+++ spec/classes/heat_keystone_auth_cfn_spec.rb
@@ -1,138 +1,81 @@
+#
+# Unit tests for heat::keystone::auth_cfn
+#
+
 require 'spec_helper'
 
 describe 'heat::keystone::auth_cfn' do
+  shared_examples_for 'heat::keystone::auth_cfn' do
+    context 'with default class parameters' do
+      let :params do
+        { :password => 'heat_password' }
+      end
 
-  let :params do
-    {:password => 'heat-passw0rd'}
-  end
-
-  shared_examples_for 'heat keystone auth' do
-
-    context 'without the required password parameter' do
-      before { params.delete(:password) }
-      it { expect { is_expected.to raise_error(Puppet::Error) } }
+      it { is_expected.to contain_keystone__resource__service_identity('heat-cfn').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'heat-cfn',
+        :service_type        => 'cloudformation',
+        :service_description => 'Openstack Cloudformation Service',
+        :region              => 'RegionOne',
+        :auth_name           => 'heat-cfn',
+        :password            => 'heat_password',
+        :email               => 'heat-cfn@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:8000/v1',
+        :internal_url        => 'http://127.0.0.1:8000/v1',
+        :admin_url           => 'http://127.0.0.1:8000/v1',
+      ) }
     end
 
     context 'when overriding parameters' do
-      before do
-        params.merge!({
-          :email              => 'heat-cfn@localhost',
-          :auth_name          => 'heat-cfn',
-          :configure_endpoint => true,
-          :configure_service  => true,
-          :service_type       => 'cloudformation',
-          :region             => 'RegionOne',
-          :tenant             => 'services',
-          :public_url         => 'http://10.0.0.10:8000/v1',
-          :admin_url          => 'http://10.0.0.11:8000/v1',
-          :internal_url       => 'http://10.0.0.12:8000/v1',
-        })
+      let :params do
+        { :password            => 'heat_password',
+          :auth_name           => 'alt_heat-cfn',
+          :email               => 'alt_heat-cfn@alt_localhost',
+          :tenant              => 'alt_service',
+          :configure_endpoint  => false,
+          :configure_user      => false,
+          :configure_user_role => false,
+          :service_description => 'Alternative OpenStack Cloudformation Service',
+          :service_name        => 'alt_service',
+          :service_type        => 'alt_cloudformation',
+          :region              => 'RegionTwo',
+          :public_url          => 'https://10.10.10.10:80',
+          :internal_url        => 'http://10.10.10.11:81',
+          :admin_url           => 'http://10.10.10.12:81'
+        }
       end
 
-      it 'configures heat user' do
-        is_expected.to contain_keystone_user( params[:auth_name] ).with(
-          :ensure   => 'present',
-          :password => params[:password],
-          :email    => params[:email],
-        )
-      end
-
-      it 'configures heat user roles' do
-        is_expected.to contain_keystone_user_role("#{params[:auth_name]}@#{params[:tenant]}").with(
-          :ensure  => 'present',
-          :roles   => ['admin']
-        )
-      end
-
-      it 'configures heat service' do
-        is_expected.to contain_keystone_service("#{params[:auth_name]}::#{params[:service_type]}").with(
-          :ensure      => 'present',
-          :description => 'Openstack Cloudformation Service'
-        )
-      end
-
-      it 'configure heat endpoints' do
-        is_expected.to contain_keystone_endpoint("#{params[:region]}/#{params[:auth_name]}::#{params[:service_type]}").with(
-          :ensure       => 'present',
-          :public_url   => params[:public_url],
-          :admin_url    => params[:admin_url],
-          :internal_url => params[:internal_url]
-        )
-      end
-
-      context 'with service disabled' do
-        before do
-          params.merge!({
-            :configure_service => false
-          })
-        end
-        it { is_expected.to_not contain_keystone_service("#{params[:auth_name]}::#{params[:service_type]}") }
-      end
-    end
-
-    context 'when overriding auth and service name' do
-      before do
-        params.merge!({
-          :auth_name => 'heat-cfny',
-          :service_name => 'heat-cfny'
-        })
-      end
-      it 'configures correct user name' do
-        is_expected.to contain_keystone_user('heat-cfny')
-      end
-      it 'configures correct user role' do
-        is_expected.to contain_keystone_user_role('heat-cfny@services')
-      end
-      it 'configures correct service name' do
-        is_expected.to contain_keystone_service('heat-cfny::cloudformation')
-      end
-      it 'configures correct endpoint name' do
-        is_expected.to contain_keystone_endpoint('RegionOne/heat-cfny::cloudformation')
-      end
-    end
-
-    context 'when disabling user configuration' do
-      before do
-        params.merge!( :configure_user => false )
-      end
-
-      it { is_expected.to_not contain_keystone_user('heat_cfn') }
-      it { is_expected.to contain_keystone_user_role('heat-cfn@services') }
-
-      it { is_expected.to contain_keystone_service('heat-cfn::cloudformation').with(
-        :ensure       => 'present',
-        :description  => 'Openstack Cloudformation Service'
-      )}
-    end
-
-    context 'when disabling user and role configuration' do
-      before do
-        params.merge!(
-          :configure_user       => false,
-          :configure_user_role  => false
-        )
-      end
-
-      it { is_expected.to_not contain_keystone_user('heat_cfn') }
-      it { is_expected.to_not contain_keystone_user_role('heat-cfn@services') }
-
-      it { is_expected.to contain_keystone_service('heat-cfn::cloudformation').with(
-        :ensure       => 'present',
-        :description  => 'Openstack Cloudformation Service'
-      )}
+      it { is_expected.to contain_keystone__resource__service_identity('heat-cfn').with(
+        :configure_user      => false,
+        :configure_user_role => false,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_cloudformation',
+        :service_description => 'Alternative OpenStack Cloudformation Service',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_heat-cfn',
+        :password            => 'heat_password',
+        :email               => 'alt_heat-cfn@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
+      ) }
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_behaves_like 'heat keystone auth'
+      it_behaves_like 'heat::keystone::auth_cfn'
     end
   end
-
 end

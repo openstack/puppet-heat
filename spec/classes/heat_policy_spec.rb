e5c3e2756dd07ae8d26afdecc4b9f70f1646905e
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:24:59 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: Ib7d19a8d3b634b3f2c88222881b175f74771de00

--- spec/classes/heat_policy_spec.rb
+++ spec/classes/heat_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'heat::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/heat/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/heat/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'heat::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/heat/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/heat/policy.d',
         )
       end

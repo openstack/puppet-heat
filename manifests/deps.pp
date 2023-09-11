57f2f332f00ea9085e5c4b05ab53f361ca9b72aa
Takashi Kajinami <tkajinam@redhat.com>
2023-09-11 12:30:49 +0900

    Do not restart services after policy file changes
    The oslo.policy library has implementations to detect change in policy
    rules and reload the new rules without service restart.
    
    Change-Id: Ie95b74bf43c2749c738286cc8a175beff5742976

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -36,7 +36,7 @@ class heat::deps {
   # policy config should occur in the config block also.
   Anchor['heat::config::begin']
   -> Openstacklib::Policy<| tag == 'heat' |>
-  ~> Anchor['heat::config::end']
+  -> Anchor['heat::config::end']
 
   # On any uwsgi config change, we must restart Heat API.
   Anchor['heat::config::begin']

aae84d93b0601c96a745f7c827f24a4e29360472
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-11-20 01:15:56 +0900

    Use new openstackclient tag
    The new openstackclient tag was added so that we can get all resources
    about openstack CLI more easily. This adds this tag to heatclient
    because the package provides some sub-commands.
    
    Also, the heatclient package is required by heat so updating it should
    restart the services.
    
    Change-Id: I4e24408d9c377d0b67d87efa07f07913ef997402

--- manifests/client.pp
+++ manifests/client.pp
@@ -14,10 +14,12 @@ class heat::client (
   include heat::deps
   include heat::params
 
+  # NOTE(tkajinam): heat-package tag is used because heatclient is required
+  #                 by heat
   package { 'python-heatclient':
     ensure => $ensure,
     name   => $::heat::params::client_package_name,
-    tag    => 'openstack',
+    tag    => ['openstack', 'openstackclient', 'heat-package'],
   }
 
   include openstacklib::openstackclient

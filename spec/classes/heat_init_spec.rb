23d4ce4367362a44b5fd455131ca6aade9255af8
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-25 21:56:55 +0900

    Add support for [oslo_messaging_rabbit] enable_cancel_on_failover
    Depends-on: https://review.opendev.org/928440
    Change-Id: Ia906577dfe896c95e9c20e35d5278a4f3f6b73cf

--- spec/classes/heat_init_spec.rb
+++ spec/classes/heat_init_spec.rb
@@ -146,6 +146,7 @@ describe 'heat' do
         :rabbit_quorum_delivery_limit    => '<SERVICE DEFAULT>',
         :rabbit_quorum_max_memory_length => '<SERVICE DEFAULT>',
         :rabbit_quorum_max_memory_bytes  => '<SERVICE DEFAULT>',
+        :enable_cancel_on_failover       => '<SERVICE DEFAULT>',
       )
     end
   end
@@ -177,6 +178,7 @@ describe 'heat' do
         :rabbit_quorum_delivery_limit    => '<SERVICE DEFAULT>',
         :rabbit_quorum_max_memory_length => '<SERVICE DEFAULT>',
         :rabbit_quorum_max_memory_bytes  => '<SERVICE DEFAULT>',
+        :enable_cancel_on_failover       => '<SERVICE DEFAULT>',
       )
     end
   end

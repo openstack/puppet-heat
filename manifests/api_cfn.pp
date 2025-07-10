8cdd00657bc14ed08cc9f76927ae367ddbd6e2d6
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-07-10 23:43:16 +0900

    Deprecate parameters for standalone api services
    ... because these were removed from heat during this cycle, due to
    removal of eventlet.
    
    Depends-on: https://review.opendev.org/952650
    Change-Id: I1b0175450385555fbd2823891cade11b1302b4a4
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/api_cfn.pp
+++ manifests/api_cfn.pp
@@ -27,24 +27,6 @@
 #   (Optional) The port on which the server will listen.
 #   Defaults to $facts['os_service_default'].
 #
-# [*workers*]
-#   (Optional) The number of workers to spawn.
-#   Defaults to $facts['os_service_default'].
-#
-# [*use_ssl*]
-#   (Optional) Whether to use ssl or not.
-#   Defaults to 'false'.
-#
-# [*cert_file*]
-#   (Optional) Location of the SSL certificate file to use for SSL mode.
-#   Required when $use_ssl is set to 'true'.
-#   Defaults to $facts['os_service_default'].
-#
-# [*key_file*]
-#   (Optional) Location of the SSL key file to use for enabling SSL mode.
-#   Required when $use_ssl is set to 'true'.
-#   Defaults to $facts['os_service_default'].
-#
 # [*service_name*]
 #   (optional) Name of the service that will be providing the
 #   server functionality of heat-api-cfn.
@@ -56,7 +38,21 @@
 #
 # == Deprecated Parameters
 #
-# No Deprecated Parameters.
+# [*workers*]
+#   (Optional) The number of workers to spawn.
+#   Defaults to undef.
+#
+# [*use_ssl*]
+#   (Optional) Whether to use ssl or not.
+#   Defaults to undef.
+#
+# [*cert_file*]
+#   (Optional) Location of the SSL certificate file to use for SSL mode.
+#   Defaults to undef
+#
+# [*key_file*]
+#   (Optional) Location of the SSL key file to use for enabling SSL mode.
+#   Defaults to undef
 #
 class heat::api_cfn (
   $package_ensure         = 'present',
@@ -64,11 +60,12 @@ class heat::api_cfn (
   Boolean $enabled        = true,
   $bind_host              = $facts['os_service_default'],
   $bind_port              = $facts['os_service_default'],
-  $workers                = $facts['os_service_default'],
-  Boolean $use_ssl        = false,
-  $cert_file              = $facts['os_service_default'],
-  $key_file               = $facts['os_service_default'],
   $service_name           = $::heat::params::api_cfn_service_name,
+  # DEPRECATED PARAMETERS
+  $workers                = undef,
+  $use_ssl                = undef,
+  $cert_file              = undef,
+  $key_file               = undef,
 ) inherits heat::params {
 
   include heat
@@ -76,12 +73,9 @@ class heat::api_cfn (
   include heat::params
   include heat::policy
 
-  if $use_ssl {
-    if is_service_default($cert_file) {
-      fail('The cert_file parameter is required when use_ssl is set to true')
-    }
-    if is_service_default($key_file) {
-      fail('The key_file parameter is required when use_ssl is set to true')
+  ['workers', 'use_ssl', 'cert_file', 'key_file'].each |String $opt| {
+    if getvar($opt) != undef {
+      warning("The ${opt} parameter is deprecated and has no effect.")
     }
   }
 
@@ -137,19 +131,11 @@ running as a standalone service, or httpd for being run by a httpd server")
   heat_config {
     'heat_api_cfn/bind_host': value => $bind_host;
     'heat_api_cfn/bind_port': value => $bind_port;
-    'heat_api_cfn/workers':   value => $workers;
   }
 
-  if $use_ssl {
-    heat_config {
-      'heat_api_cfn/cert_file': value => $cert_file;
-      'heat_api_cfn/key_file':  value => $key_file;
-    }
-  } else {
-    heat_config {
-      'heat_api_cfn/cert_file': value => $facts['os_service_default'];
-      'heat_api_cfn/key_file':  value => $facts['os_service_default'];
-    }
+  heat_config {
+    'heat_api_cfn/workers':   ensure => absent;
+    'heat_api_cfn/cert_file': ensure => absent;
+    'heat_api_cfn/key_file':  ensure => absent;
   }
-
 }

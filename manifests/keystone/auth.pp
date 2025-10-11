33a095e91a4ddbac3e4d379f3d145e05977ddb80
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-10-11 11:55:23 +0900

    Add service role by default
    This is the role created to be used by service users. At this moment
    several services/functionalities still need the admin role so is kept.
    
    Change-Id: I1b06f11a481cf58ff43ec2ab65423a224eb82904
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -52,7 +52,7 @@
 #
 # [*roles*]
 #   (Optional) List of roles assigned to heat user.
-#   Defaults to ['admin']
+#   Defaults to ['admin', 'service']
 #
 # [*system_scope*]
 #   (Optional) Scope for system operations.
@@ -117,7 +117,7 @@ class heat::keystone::auth (
   String[1] $service_description           = 'OpenStack Orchestration Service',
   String[1] $region                        = 'RegionOne',
   String[1] $tenant                        = 'services',
-  Array[String[1]] $roles                  = ['admin'],
+  Array[String[1]] $roles                  = ['admin', 'service'],
   String[1] $system_scope                  = 'all',
   Array[String[1]] $system_roles           = [],
   Boolean $configure_endpoint              = true,

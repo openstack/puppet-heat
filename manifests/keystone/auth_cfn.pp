3fb01fedb4ab34be8f8f5a5372a66276deb3a0b3
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-05-29 00:08:47 +0900

    Remove parameters for dedicated heat-cfn keystone user
    These were deprecated during the past cycle[1].
    
    [1] 0beecf38725f4039da56e39c591b70532fe08ae9
    
    Change-Id: I16d3136b60995c7819508c07072f0a7a24e6a129
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/keystone/auth_cfn.pp
+++ manifests/keystone/auth_cfn.pp
@@ -40,44 +40,6 @@
 #   (optional) The endpoint's internal url. (Defaults to 'http://127.0.0.1:8000/v1')
 #   This url should *not* contain any trailing '/'.
 #
-# DEPRECATED PARAMETERS
-#
-# [*configure_user*]
-#   (Optional) Whether to create the service user.
-#   Defaults to undef
-#
-# [*configure_user_role*]
-#   (Optional) Whether to configure the admin role for the service user.
-#   Defaults to undef
-#
-# [*password*]
-#   (Optioal) Password for heat-cfn user.
-#   Defaults to undef
-#
-# [*email*]
-#   (Optional) Email for heat-cfn user.
-#   Defaults to undef
-#
-# [*auth_name*]
-#   (Optional) Username for heat-cfn service.
-#   Defaults to undef
-#
-# [*tenant*]
-#   (Optional) Tenant for heat-cfn user.
-#   Defaults to undef
-#
-# [*roles*]
-#   (Optional) List of roles assigned to heat user.
-#   Defaults to undef
-#
-# [*system_scope*]
-#   (Optional) Scope for system operations.
-#   Defaults to undef
-#
-# [*system_roles*]
-#   (Optional) List of system roles assigned to heat user.
-#   Defaults to undef
-#
 # === Examples
 #
 #  class { 'heat::keystone::auth_cfn':
@@ -96,32 +58,8 @@ class heat::keystone::auth_cfn (
   Keystone::PublicEndpointUrl $public_url = 'http://127.0.0.1:8000/v1',
   Keystone::EndpointUrl $admin_url        = 'http://127.0.0.1:8000/v1',
   Keystone::EndpointUrl $internal_url     = 'http://127.0.0.1:8000/v1',
-  # DEPRECATED PARAMETERS
-  Optional[Boolean] $configure_user       = undef,
-  Optional[Boolean] $configure_user_role  = undef,
-  $password                               = undef,
-  $email                                  = undef,
-  $auth_name                              = undef,
-  $tenant                                 = undef,
-  $roles                                  = undef,
-  $system_scope                           = undef,
-  $system_roles                           = undef,
 ) {
   include heat::deps
-
-  if $configure_user != undef {
-    warning('The configure_user parameter is deprecated')
-    if $configure_user {
-      fail('Management of heat-cfn user is no longer supported.')
-    }
-  }
-
-  if $configure_user_role != undef {
-    warning('The configure_user_role parameter is deprecated')
-    if $configure_user_role {
-      fail('Management of heat-cfn user is no longer supported.')
-    }
-  }
 
   Keystone::Resource::Service_identity['heat-cfn'] -> Anchor['heat::service::end']
 
